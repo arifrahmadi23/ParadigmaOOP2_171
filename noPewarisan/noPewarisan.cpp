be049b0dd48fc2fd9f7624ba05ca9dfdd66329c2
arifrahmadi23 <muhammad231arif@gmail.com>
2023-05-29 17:10:11 +0700

    memperbaiki (salah pengetikan)

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -2,14 +2,3 @@
 using namespace std;
 
 class baseClass final {
-private:
-	string saklarNo[10];
-public:
-	void setSaklarNo(int i, string value) {
-		saklarNo[i] = value;
-	}
-	string getSaklarNo(int i) {
-		return saklarNo[i];
-	}
-};
-
