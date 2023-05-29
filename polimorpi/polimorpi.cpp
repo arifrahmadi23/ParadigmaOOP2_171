3e7fa6a6cd071b23dc0ef3571d84307ddd95be48
arifrahmadi23 <muhammad231arif@gmail.com>
2023-05-29 15:57:33 +0700

    int main

--- polimorpi/polimorpi.cpp
+++ polimorpi/polimorpi.cpp
@@ -24,3 +24,15 @@ public:
 
 };
 
+int main() {
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+	//a.seseorang::pesan();
+	//b.seseorang::pesan();
+}
